093e0abeb04bde9db1f39eb4d003049671fc7535
Deimos <alexjbarnier@gmail.com>
2019-08-31 12:14:46 +1000

    added basic metric and event emitter

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,30 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import * as EventEmitter from 'events';
+
+let emitter = new EventEmitter();
+
+let delta:number = 0;
+let count:number = 0;
+
+setInterval(()=>{
+	let editor = vscode.window.activeTextEditor;
+	if(!editor) return;
+	let currentDoc = editor.document;
+	let diags = vscode.languages.getDiagnostics();
+	let temp:number  =currentDoc.getText().length;
+	for(let i:number = 0;i<diags.length;++i){
+		for(let j:number = 0;j<diags[i][1].length;++j){
+			if(vscode.DiagnosticSeverity[diags[i][1][j].severity]==="Error"){
+				temp -= diags[i][1][j].range.end.character-diags[i][1][j].range.start.character;
+			}
+		}
+	}
+	delta = temp - count;
+	count = temp;
+	emitter.emit('ecg-change',delta);
+},500);
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
